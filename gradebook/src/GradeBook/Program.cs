4bdc68fb59e710a83249002954712c204b14f71e
Rehan Memon <rehan4747@gmail.com>
2021-02-01 17:33:46 +0530

    Completed Controlling the Flow of Execution Module

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -9,9 +9,43 @@ namespace GradeBook
         static void Main(string[] args)
         {   
             var book = new Book("Rehan's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+            // var done= false;
+
+            // while(!done)
+            while(true)
+            {
+                Console.WriteLine("Enter A Grade Or 'Q' to Quit");
+                var input = Console.ReadLine();
+
+                if (input == "Q")
+                {
+                    // done = true;
+                    // continue;
+                    break;
+                }
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                } 
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    // throw;
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+                
+            }
+            // book.AddGrade(89.1);
+            // book.AddGrade(90.5);
+            // book.AddGrade(77.5);
             
             var stats = book.GetStatistics();     
 
@@ -19,6 +53,7 @@ namespace GradeBook
             Console.WriteLine($"The Average Grade is {stats.Average:N1} !");
             Console.WriteLine($"The Highest Grade is {stats.High:N1} !");
             Console.WriteLine($"The Lowest Grade is {stats.Low:N1} !");
+            Console.WriteLine($"The Letter Grade is {stats.Letter} !");
         }
     }
 }
--- gradebook/test/GradeBook.Tests/BookTests.cs
+++ gradebook/test/GradeBook.Tests/BookTests.cs
@@ -10,17 +10,19 @@ namespace GradeBook.Tests
         {
             // Arrange
             var book = new Book("");
+
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.3);
             
-             //Act
+            //Act
             var result = book.GetStatistics(); 
 
             //Assert
             Assert.Equal(85.6, result.Average, 1);
             Assert.Equal(90.5, result.High, 1);
             Assert.Equal(77.3, result.Low, 1);
+            Assert.Equal('B', result.Letter);
         }
     }
 }
